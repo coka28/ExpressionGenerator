59086e36466daf5f9e3a5ceb4264c373c6d8a109
coka28 <70796761+coka28@users.noreply.github.com>
2021-03-02 04:46:27 +0100

    Add files via upload

--- exprgen.py
+++ exprgen.py
@@ -24,48 +24,37 @@ def make_expr(difficulty=5):
         ]
 
     wrap = lambda : wrappers[rint(0,len(wrappers)-1)]
+    global x,y
+    x = difficulty
+    y = 0
 
-    x = 1
-    expr = []
+    def add():
+        global x,y
+        x -= rnd()
+        if rnd()<1/difficulty or x<0:
+            if y>0: return rint(0,9) + (round(rnd(),1) if rnd()<0.1 else 0)
+            else: pass
+        y += 1
 
-    while x>0:
-        x -= rnd()**difficulty
-        expr.append(rint(0,len(wrappers)-1))
-
-    while len(expr)>1:
-        i = rint(0,len(expr)-1)
-        a,b = None,None
         f = wrap()
-        delete = []
-        if i>0 and isinstance(expr[i-1],str):
-            a = expr[i-1]
-            delete.append(i-1)
-        else:
-            a = rint(0,10)
-            if rnd()>0.8: a += round(rnd(),1)
-        if args>1 and i<len(expr)-1 and isinstance(expr[i+1],str):
-            b = expr[i+1]
-            delete.append(i+1)
-        else:
-            b = rint(0,10)
-            if rnd()>0.8: b += round(rnd(),1)
+        args = f.__code__.co_argcount
+        return f(*(add(),add())[:args])
 
-        expr[i] = f(*(a,b)[:f.__code__.co_argcount])
-        for k in delete[::-1]:
-            del expr[k]
-
-    return expr[0]
+    return str(add())
+        
 
 
 def testme(questions=10,incr=1):
-    questions_ = [make_expr(i) for i in range(incr,questions*incr+1,incr)]
+    questions_ = [make_expr(incr*i) for i in range(1,questions+1)]
     correct = 0
     for q in range(len(questions_)):
         print()
         print('%s. Frage:'%(q+1))
         print(questions_[q])
-        answer = input('What does it evaluate to?\n').replace(' ','').replace('\t','')
-        try: correct_answer = str(eval(questions_[q]))
+        answer = input('What does it evaluate to?\n')
+        try:
+            correct_answer = str(eval(questions_[q]))
+            answer = answer.replace(' ','').replace('\t','')
         except Exception as e:
             correct_answer = str(e)
         if correct_answer == answer:
